8f7b3dd025e4073d833fdc4887585d11b25649e8
nekoKaori <cheerlee428@gmail.com>
2018-09-11 01:02:01 -0400

    The test was to see if it was getting the right selectors and it is. Added CSS to table. Started to get genetic code for each parent.

--- turtles.js
+++ turtles.js
@@ -1,4 +1,6 @@
 var tablePopulation = "";
+var females = "";
+var males = "";
 var turtles = [
 {turtleId:001, turtleName:"Mom", turtleGender:"Girl", turtlePicture:"Images/turtleBasic.png", turtleGenetics:"AABBCCDDEE"},
 {turtleId:002, turtleName:"Dad", turtleGender:"Boy", turtlePicture:"Images/turtleBasic.png", turtleGenetics:"ABBCCDDEEE"}];
@@ -6,9 +8,24 @@ var turtles = [
 function populateTable(){
 	tablePopulation = '<thead><tr><td>Turtle ID Number</td><td>Turtle Name</td><td>Turtle Gender</td><td>Turtle Picture</td></tr></thead><tbody>';
 	tLen = turtles.length;
-	for (i = 0; i < tLen; i++) {
-	tablePopulation += '<tr> <td>' + turtles[i].turtleId + '</td><td>' + turtles[i].turtleName + '</td><td>' + turtles[i].turtleGender + '</td><td><img src="' + turtles[i].turtlePicture + '" style="width:252px;height:252px;"></td></tr>';
-	}
+		for (i = 0; i < tLen; i++) {
+		tablePopulation += '<tr> <td>' + turtles[i].turtleId + '</td><td>' + turtles[i].turtleName + '</td><td>' + turtles[i].turtleGender + '</td><td><img src="' + turtles[i].turtlePicture + '" style="width:252px;height:252px;"></td></tr>';
+			if (turtles[i].turtleGender == "Girl"){
+				females += '<option value="' + turtles[i].turtleId + '">' + turtles[i].turtleName + '</option>';
+			} else {
+				males += '<option value="' + turtles[i].turtleId + '">' + turtles[i].turtleName + '</option>';
+			}		
+		}
+	document.getElementById("femaleList").innerHTML = females;
+	document.getElementById("maleList").innerHTML = males;
 	tablePopulation += '</tbody>';
 	document.getElementById("turtleList").innerHTML = tablePopulation;
 }
+
+function breed(){
+	var female = document.getElementById("femaleList").value - 1;
+	var male = document.getElementById("maleList").value - 1 ;
+	document.getElementById("test").innerHTML = "The mom will be " + female + " and the dad will be " + male;
+	var femaleGenetics = turtles[female].turtleGenetics;
+	var maleGenetics = turtles[male].turtleGenetics;
+	}
