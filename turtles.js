f3103c40c276a6f31646f03a48270e9df820dd21
nekoKaori <cheerlee428@gmail.com>
2018-09-11 22:21:21 -0400

    It works! Genesplicing now only continues randomingly splicing if the pair before it are recessive. it sits in a do while loop. Rest is generated with one from each parent.

--- turtles.js
+++ turtles.js
@@ -5,14 +5,20 @@ var females = "";
 var males = "";
 //turtle family
 var turtles = [
-{turtleId:1, turtleName:"Mom", turtleGender:"Girl", turtlePicture:"Images/turtleBasic.png", turtleGenetics:"AABBCCDDEE"},
-{turtleId:2, turtleName:"Dad", turtleGender:"Boy", turtlePicture:"Images/turtleBasic.png", turtleGenetics:"ABBCCDDEEE"}];
-
+{turtleId:1, turtleName:"Mom", turtleGender:"Girl", turtlePicture:"Images/turtleBasic.png", turtleGenetics:"AaBbCcDdEe"},
+{turtleId:2, turtleName:"Dad", turtleGender:"Boy", turtlePicture:"Images/turtleBasic.png", turtleGenetics:"AaBbCcDdEe"}];
+//variables moved to global to try and make them work
+var childGender = '';
+var childGenetics;
+var babypicture;
 
 //Onload the table and dropdown selectors are populated based off turtle family
 function populateTable(){
 	tablePopulation = '<thead><tr><td>Turtle ID Number</td><td>Turtle Name</td><td>Turtle Gender</td><td>Turtle Picture</td></tr></thead><tbody>';
 	tLen = turtles.length;
+	//reset list before populating
+	females = "";
+	males = "";
 		for (i = 0; i < tLen; i++) {
 		tablePopulation += '<tr> <td>' + turtles[i].turtleId + '</td><td>' + turtles[i].turtleName + '</td><td>' + turtles[i].turtleGender + '</td><td><img src="' + turtles[i].turtlePicture + '" style="width:252px;height:252px;"></td></tr>';
 			if (turtles[i].turtleGender == "Girl"){
@@ -35,17 +41,19 @@ function breed(){
 	var femaleGenetics = turtles[female].turtleGenetics;
 	var maleGenetics = turtles[male].turtleGenetics;
 	//sets values for child attributes
-	var childGenetics = geneSplice(femaleGenetics, maleGenetics);
-	var childGender = '';
+	//var childGenetics = geneSplice(femaleGenetics, maleGenetics);
+	childGenetics = geneSplice(femaleGenetics, maleGenetics);
+	//var childGender = '';
 	var randomGender = Math.floor((Math.random() * 10));
 	if (randomGender <= 4) {
 			childGender = "Girl";
 		} else {
 			childGender = "Boy";
 		}
-	var babypicture = getBabyPicture(childGenetics);	
+	//var babypicture = getBabyPicture(childGenetics);	
+	babypicture = getBabyPicture(childGenetics);	
 	//gets displays baby info and gets name input from user	
-	document.getElementById("nameTheChild").innerHTML = "<form> Congrats, Baby Turtle is a " + childGender + "!<br>Please Enter Baby's Name: " + '<input type="text" name="name" value="Baby ' + childGender + '"> <button type="button" onclick="addBaby()">Submit</button></form><br>'
+	document.getElementById("nameTheChild").innerHTML = "<form> Congrats, Baby Turtle is a " + childGender + "!<br>Please Enter Baby's Name: " + '<input type="text" name="name" id="babyName" value="Baby ' + childGender + '"> <button type="button" onclick="addBaby()">Submit</button></form><br>'
 	+ childGenetics + '<img src="' + babypicture + '" style="width:126px;height:126px;">'; 
 	}
 
@@ -57,7 +65,8 @@ function geneSplice(sf, sm){
 	var child = "";
 	var gLen = setOne.length;
 	var babyGenetics = "";
-	for (i=0; i<gLen; i+=2){
+	var i = 0;
+	do {
 		var ranNumA = Math.floor((Math.random() * 10));
 		var ranNumB = Math.floor((Math.random() * 10));
 		if (ranNumA <= 4) {
@@ -70,6 +79,12 @@ function geneSplice(sf, sm){
 		} else {
 			setChild.push(setTwo[i+1]);
 		}
+		i += 2;
+	}
+	while (setChild[i-2] == setChild[i-1] && setChild[i-2] == setChild[i-2].toLowerCase());	
+	for (i; i<gLen; i+=2) {
+				setChild.push(setOne[i]);
+				setChild.push(setTwo[i+1]);
 	}
 	gLen = setChild.length;
 	babyGenetics = setChild.toString();
@@ -108,8 +123,9 @@ function getBabyPicture(childGens){
 
 //Adding the new child to the family
 function addBaby(){
+	var childName = document.getElementById("babyName").value
 	document.getElementById("nameTheChild").innerHTML = "";
 	tLen = turtles.length +1;
-	turtles.push({turtleId:tLen, turtleName:"Mom", turtleGender:childGender, turtlePicture:babypicture, turtleGenetics:childGenetics});
+	turtles.push({turtleId:tLen, turtleName:childName, turtleGender:childGender, turtlePicture:babypicture, turtleGenetics:childGenetics});
 	populateTable();
 }
